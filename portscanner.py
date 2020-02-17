bdc4665da77f453e84b9f4ea62ab6931b977df1b
Echo <47682300+Echo361@users.noreply.github.com>
2020-02-17 11:33:44 +1100

    Update portscanner.py
    Completed script, Ready for testing

--- portscanner.py
+++ portscanner.py
@@ -21,5 +21,24 @@ try:
         if result ==0:
             print "Port {}: Open".format(port)
         sock.close()
-        
-       #TO BE CONTINUED
+
+except KeyboardInterrupt:
+    print "You pressed Ctrl+C"
+    sys.exit()
+
+except socket.gaierror:
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+except socket.error:
+    print "Couldn't connect to server"
+    sys.exit()
+
+
+t2 = datetime.now()
+
+
+total =  t2 - t1
+
+
+print 'Scanning Completed in: ', total
